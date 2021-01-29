8856a768821b1f7a97d3321c20af781e07cb4cbc
Mateusz <mateuszklonica@gmail.com>
2021-01-29 23:41:09 +0100

    Creating headerLogo + navigation features

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const userNameInput = document.querySelector("#name");
 const userPhoneNumber = document.querySelector("#phone");
 const nameValid = new RegExp(/^[A-Za-ząćęńłźżóŁ]{3,}\s+[A-Za-ząćęńłźżóŁŻ]{3,}$/, "m"); // first name and surname validator 
-const phoneValid = new RegExp(/^(\(?(\+|00)?48\)?)?[ -]?[1-9]{1}\d{2}[ -]?\d{3}[ -]?\d{3}$/) // Polish phone number validator
-const emailValid = new RegExp(/^.+@[^/. ].*\.[a-z]{2,}$/)
-
+const phoneValid = new RegExp(/^(\(?(\+|00)?48\)?)?[ -]?[1-9]{1}\d{2}[ -]?\d{3}[ -]?\d{3}$/); // Polish phone number validator
+const emailValid = new RegExp(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+//Soon rebuild to function = return Object.RegExp.test(String( expression).toLowerCase());
 function inputBoolenValidator(regexPattern, elementTarget) {
     const domClassListArray = [...elementTarget.classList];
     if (domClassListArray.length > 1) {
@@ -43,3 +43,66 @@ toContactForm.addEventListener("click", () => {
     }
 })
 // to appear and disappear contact form
+const photoslider = document.getElementById("slider");
+const listPhoto = photoslider.children;
+const nextButton = document.querySelector("#next");
+const prevButton = document.querySelector("#prev");
+let count = 0;
+prevButton.addEventListener("click", () => { prevSlide() });
+nextButton.addEventListener("click", () => { nextSlide() });
+function nextSlide() {
+    listPhoto[count].classList.remove("activeSlide");
+    if (count === listPhoto.length - 1)
+        count = 0;
+    else
+        count += 1;
+    listPhoto[count].classList.add("activeSlide");
+}
+function prevSlide() {
+    listPhoto[count].classList.remove("activeSlide");
+    if (count === 0)
+        count = listPhoto.length - 1;
+    else
+        count -= 1;
+    listPhoto[count].classList.add("activeSlide");
+}
+// slider
+const linkUL = document.getElementById("linkList");
+const hamburger = document.querySelector("#hamburger");
+const setBtnState = document.querySelectorAll(".burger")
+let classes = linkUL.classList;
+hamburger.classList.add("beng");
+hamburger.addEventListener("click", () => {
+    let countBar = 1;
+    let result = classes.toggle("disappearContact");
+    if (result) {
+        console.log("Off");
+        hamburger.classList.remove("beng");
+    }
+    else {
+        console.log("ON")
+        hamburger.classList.add("beng");
+    }
+
+    if (linkUL.style.display === "none") {
+        linkUL.classList.remove("disappearContact");
+        linkUL.classList.add("appearContact");
+        linkUL.style = "";
+    }
+    else {
+        linkUL.classList.remove("appearContact");
+        linkUL.classList.add("disappearContact");
+        linkUL.style.display = "none";
+    }
+})
+// navbarMenu button
+function contains(a, obj) {
+    for (var i = 0; i < a.length; i++) {
+        if (a[i] === obj) {
+            return true;
+        }
+    }
+    return false;
+}
+
+//does it nice ?

b80a0fec0062e17ff206f3302bd886c67fa284ea
Mateusz <mateuszklonica@gmail.com>
2021-02-15 17:00:06 +0100

    New responsive navigation bar

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const userPhoneNumber = document.querySelector("#phone");
 const nameValid = new RegExp(/^[A-Za-ząćęńłźżóŁ]{3,}\s+[A-Za-ząćęńłźżóŁŻ]{3,}$/, "m"); // first name and surname validator 
 const phoneValid = new RegExp(/^(\(?(\+|00)?48\)?)?[ -]?[1-9]{1}\d{2}[ -]?\d{3}[ -]?\d{3}$/); // Polish phone number validator
 const emailValid = new RegExp(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
-//Soon rebuild to function = return Object.RegExp.test(String( expression).toLowerCase());
+//Soon rebuild to function = return Object.RegExp.test(String( expression).toLowerCase()); 
 function inputBoolenValidator(regexPattern, elementTarget) {
     const domClassListArray = [...elementTarget.classList];
     if (domClassListArray.length > 1) {
@@ -20,13 +20,13 @@ function inputBoolenValidator(regexPattern, elementTarget) {
     }
 }
 //funtion valid user's input than check string is valid or invalid
-userNameInput.addEventListener("change", () => { inputBoolenValidator(nameValid, userNameInput) });
-userPhoneNumber.addEventListener("change", () => { inputBoolenValidator(phoneValid, userPhoneNumber) });
+// userNameInput.addEventListener("change", () => { inputBoolenValidator(nameValid, userNameInput) });
+// userPhoneNumber.addEventListener("change", () => { inputBoolenValidator(phoneValid, userPhoneNumber) });
 //
-const formButton = document.querySelector("#sendMessage");
-formButton.addEventListener("click", (event) => {
-    event.preventDefault();
-})
+// const formButton = document.querySelector("#sendMessage");
+// formButton.addEventListener("click", (event) => {
+//     event.preventDefault();
+// })
 // form validation
 const toContactForm = document.querySelector("#tocantackForm");
 toContactForm.addEventListener("click", () => {
@@ -43,58 +43,63 @@ toContactForm.addEventListener("click", () => {
     }
 })
 // to appear and disappear contact form
-const photoslider = document.getElementById("slider");
-const listPhoto = photoslider.children;
-const nextButton = document.querySelector("#next");
-const prevButton = document.querySelector("#prev");
-let count = 0;
-prevButton.addEventListener("click", () => { prevSlide() });
-nextButton.addEventListener("click", () => { nextSlide() });
-function nextSlide() {
-    listPhoto[count].classList.remove("activeSlide");
-    if (count === listPhoto.length - 1)
-        count = 0;
-    else
-        count += 1;
-    listPhoto[count].classList.add("activeSlide");
-}
-function prevSlide() {
-    listPhoto[count].classList.remove("activeSlide");
-    if (count === 0)
-        count = listPhoto.length - 1;
-    else
-        count -= 1;
-    listPhoto[count].classList.add("activeSlide");
-}
+// const photoslider = document.getElementById("slider");
+// const listPhoto = photoslider.children;
+// const nextButton = document.querySelector("#next");
+// const prevButton = document.querySelector("#prev");
+// let count = 0;
+// prevButton.addEventListener("click", () => { prevSlide() });
+// nextButton.addEventListener("click", () => { nextSlide() });
+// function nextSlide() {
+//     listPhoto[count].classList.remove("activeSlide");
+//     if (count === listPhoto.length - 1)
+//         count = 0;
+//     else
+//         count += 1;
+//     listPhoto[count].classList.add("activeSlide");
+// }
+// function prevSlide() {
+//     listPhoto[count].classList.remove("activeSlide");
+//     if (count === 0)
+//         count = listPhoto.length - 1;
+//     else
+//         count -= 1;
+//     listPhoto[count].classList.add("activeSlide");
+// }
 // slider
 const linkUL = document.getElementById("linkList");
+
+let classes = linkUL.classList;
+
 const hamburger = document.querySelector("#hamburger");
 
 const setBtnState = document.querySelectorAll(".burger")
-let classes = linkUL.classList;
-hamburger.classList.add("beng");
+const iconBars = [...setBtnState];
 
+hamburger.classList.add("beng");
+// display animation fix ...
 let countBar = 1;
 hamburger.addEventListener("click", () => {
-    let result = classes.toggle("bang");
+    let result = classes.toggle("beng");
     if (result) {
-        // console.log("Off");
         hamburger.classList.remove("beng");
-        hamburger.classList.add("bengShift");
-        [...setBtnState].forEach(element => {
+        iconBars.forEach(element => {
             element.classList.add(`bar${countBar++}`);
         });
+        hamburger.classList.add("bengShift");
+        // classes.add("")
         linkUL.style.display = "none"
     }
     else {
-        // console.log("ON")
-        hamburger.classList.add("beng");
         hamburger.classList.remove("bengShift");
-        countBar = 1;
+        iconBars.forEach(element => {
+            element.classList.remove(`bar${countBar--}`);
+        });
+        hamburger.classList.add("beng");
         linkUL.style = "";
     }
 })
 // navbarMenu button
 
 //Help imporve my code send feedback :)
-//does it nice ?
+
